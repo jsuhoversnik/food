c90ac5f739d1b66b8f83b0ebe545dcb8bcb99f65
jsuhoversnik <jake.suhoversnik@gmail.com>
2019-01-14 13:36:44 -0500

    setup views

--- index.php
+++ index.php
@@ -20,9 +20,9 @@ $f3->set('DEBUG',3);
 
 //define a default route
 $f3->route('GET /', function(){
-    echo '<h1>My Favorite Foods</h1>';
-    //$view = new View;
-    //echo $view->render('views/home.html');
+    //echo '<h1>My Favorite Foods</h1>';
+    $view = new View();
+    echo $view->render('views/home.html');
 });
 
 //run fat free
