0de6f624aa6498e0f17c686e369357375f98a42c
jsuhoversnik <jake.suhoversnik@gmail.com>
2019-01-23 13:49:25 -0500

    added variables and template

--- index.php
+++ index.php
@@ -19,22 +19,47 @@ $f3 = Base::instance();
 $f3->set('DEBUG',3);
 
 //define a default route
-$f3->route('GET /', function(){
+$f3->route('GET /', function($f3){
+
+    //save variables
+    $f3->set('username','jshmo');
+    $f3->set ('password',sha1('Password01'));
+    $f3->set('title','Working with Templates');
+    $f3->set('temp',67);
+    $f3->set('color','purple');
+    $f3->set('radius',10);
+    $f3->set('fruits',array('apple','orange','banana'));
+
+    $f3->set('bookmarks',array('cnn'=>'http://www.cnn.com',
+                                'google'=>'http://www.google.com'));
+
+    //load a template
+    $template = new Template();
+    echo $template ->render('views/info.html');
+    //alternate syntax
+    //echo Template::instance()->render('views/info.html');
+
     //echo '<h1>My Favorite Foods</h1>';
-    $view = new View();
-    echo $view->render('views/home.html');
+    //$view = new View();
+    //echo $view->render('views/home.html');
 });
 
 //Define a breakfast route
-$f3->route('GET /breakfast', function(){
+$f3->route('GET /Breakfast', function(){
     $view = new View();
     echo $view->render('views/breakfast.html');
 });
 
 //Define a lunch route
-$f3->route('GET /lunch', function(){
+$f3->route('GET /Lunch', function(){
     $view = new View();
     echo $view->render('views/lunch.html');
+});
+
+//Define a lunch route
+$f3->route('GET /dinner', function(){
+    $view = new View();
+    echo $view->render('views/dinner.html');
 });
 
 
@@ -42,6 +67,20 @@ $f3->route('GET /lunch', function(){
 $f3->route('GET /breakfast/pancakes', function(){
     $view = new View();
     echo $view->render('views/pancakes.html');
+});
+
+//Define dinner food routes
+$f3->route('GET /dinner/burgers', function(){
+    $view = new View();
+    echo $view->render('views/burgers.html');
+});
+$f3->route('GET /dinner/tacos', function(){
+    $view = new View();
+    echo $view->render('views/tacos.html');
+});
+$f3->route('GET /dinner/teriyaki', function(){
+    $view = new View();
+    echo $view->render('views/teriyaki.html');
 });
 
 //run fat free
